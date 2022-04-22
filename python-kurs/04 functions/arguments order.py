77d57fd990f182dd8a0735cad52c9e2af66a2ac5
Jakub Samul <jakubsamul0@gmail.com>
2022-04-22 21:31:59 +0200

    c.d.

---
+++ python-kurs/04 functions/arguments order.py
@@ -0,0 +1,10 @@
+
+def printCar(brand,/, name="concept",*,year=1960,color="black"):
+    print(brand,name,year,color)
+
+printCar("Ford","Mustang",year=1973,color="Blue")
+printCar("Ford","Mustang",color="Blue",year=1973)
+printCar("Ford",name="Mustang",color="Blue",year=1973)
+# printCar("Ford",name="Mustang",color="Blue",1973) błąd
+# printCar("Ford",name="Mustang","Blue",year=1973) błąd
+# printCar(brand="Ford","Mustang",color="Blue",year=1973) błąd
