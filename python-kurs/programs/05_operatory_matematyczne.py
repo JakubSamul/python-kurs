b6fab61f83eb5c100cd5edea82b26d1a0498c3ff
Jakub Samul <jakubsamul0@gmail.com>
2022-05-11 11:43:14 +0200

    c.d.

---
+++ python-kurs/programs/05_operatory_matematyczne.py
@@ -0,0 +1,28 @@
+
+a = 4.0
+b = 2
+
+print(a-b)
+print(a+b)
+print(a*b)
+print(a/b)
+
+print(type(a-b))
+print(type(a+b))
+print(type(a*b))
+print(type(a/b))
+
+a = 5
+b = 3
+
+print(a**b)
+
+a = a + 1
+
+print(a)
+
+a += 1
+
+print(a)
+
+#a /= 2 - nie zadziała bo jest zły typ trzeba zmienić a na float
---
+++ python-kurs/programs/06_operatory_porównania.py
@@ -0,0 +1,19 @@
+
+a = 5
+b = 5
+
+print(a == 5)
+print(a != 5)
+print(a != 10)
+print(a > 5)
+print(a < 5)
+print(a <= 5)
+print(a >= 5)
+
+print(a == b)
+print(a != b)
+
+print(a is b) # is porównuje adresy w pamięci 
+
+
+
